15205fc3ab4aba67d5c1dff416642dd6ca271330
James <13262021+james-coder@users.noreply.github.com>
2020-05-21 15:31:05 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-if [[ "$*" == "" ]]; then
-	echo "Please specify paths in your repo to run Perl Critic on"
-    	exit 1
-fi
+which reviewdog
+which perl
+which perlcritic
 
-if [[ "$*" == "git-diff" ]]; then
-    echo "Using SHA: $GITHUB_SHA"
-    FILES=`git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | grep -e "\.pm$" -e "\.pl$" -e "\.cgi$"`
-    violations=$(perlcritic $FILES)
-else
-    violations=$(perlcritic $*)
-fi
+reviewdog --version
+perl --version
+perlcritic --version
 
-success=$?
-echo "$violations"
 
-if [ $success -ne 0 ]; then
-    #Report the critic violations in a comment on the commit
+# if [[ "$*" == "" ]]; then
+# 	echo "Please specify paths in your repo to run Perl Critic on"
+#     	exit 1
+# fi
 
-    COMMENT="#### Perl Critic Notes (Level 5 - gentle):
-<pre>$violations</pre>"
-    PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-    COMMIT_URL="https://api.github.com/repos/"$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments
-    echo "Pushing payload to $COMMIT_URL"
-    curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMIT_URL" > /dev/null
+# if [[ "$*" == "git-diff" ]]; then
+#     echo "Using SHA: $GITHUB_SHA"
+#     FILES=`git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | grep -e "\.pm$" -e "\.pl$" -e "\.cgi$"`
+#     violations=$(perlcritic $FILES)
+# else
+#     violations=$(perlcritic $*)
+# fi
 
-    exit 1
-fi
+# success=$?
+# echo "$violations"
 
-exit 0
+# if [ $success -ne 0 ]; then
+#     #Report the critic violations in a comment on the commit
+
+#     COMMENT="#### Perl Critic Notes (Level 5 - gentle):
+# <pre>$violations</pre>"
+#     PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+#     COMMIT_URL="https://api.github.com/repos/"$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments
+#     echo "Pushing payload to $COMMIT_URL"
+#     curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMIT_URL" > /dev/null
+
+#     exit 1
+# fi
+
+# exit 0
