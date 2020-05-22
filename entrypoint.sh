2a19de779c2d2a991f36463b1ca6fd8a33843b22
James <13262021+james-coder@users.noreply.github.com>
2020-05-22 01:24:12 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,20 +19,19 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 export PERL5LIB="${GITHUB_WORKSPACE}/modules"
 
 echo "## Running perlcritic"
-perlcritic --gentle --profile /.perlcriticrc modules/NFFS/*.pm |
-   reviewdog -name="perlcritic" -filter-mode=file -efm="%f:%l:%m" -reporter="github-pr-check"
+find . -regex '.*\.\(pl\|pm\)' -exec perlcritic --gentle --profile /.perlcriticrc {} \; |
+   reviewdog -name="perlcritic" -filter-mode=file -efm="%f:%l:%c:%m" -reporter="github-pr-check"
 
-export ESC_GITHUB_WORKSPACE=$(echo "$GITHUB_WORKSPACE" | perl -pe 's/\//\\\//g')
+#export ESC_GITHUB_WORKSPACE=$(echo "$GITHUB_WORKSPACE" | perl -pe 's/\//\\\//g')
 
 # SUBSTR below puts the "perl -c format" into "file:line:error" format for reviewdog.
 # (Also trims ./ or ../../ or /somedir/ from beginning of file path.)
-#export SUBSTR="s/(.*) at (\.\/|\.\.\/\.\.\/|$ESC_GITHUB_WORKSPACE)(.*) line (\d+)(.*)/\$3:\$4:\$1/g"
 export SUBSTR="s/(.*) at (.\/|\/github\/workspace\/)(.*) line (\d+)(.*)/\$3:\$4:\$1/g"
 
 echo "## Running perl -c (on *.pm)"
-find . -name \*.pm -exec perl -c {} 2>&1 \; |
+find . -regex '.*\.\(pl\|pm\)' -exec perl -c {} 2>&1 \; |
    grep -v " syntax OK" |
    perl -pe "$SUBSTR" |
-   reviewdog -name="perl-syntax" -efm="%f:%l:%m" -reporter="github-pr-check"
+   reviewdog -name="perl-syntax" -filter-mode=file  -efm="%f:%l:%m" -reporter="github-pr-check"
 
 # "\.pm$" -e "\.pl$" -e "\.cgi$"`
