235b17f20efef2c1657c2c8f567cb48f8547405d
James <13262021+james-coder@users.noreply.github.com>
2020-05-22 03:34:53 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,32 +11,32 @@ perlcritic --version
 echo "## cpanm -V"
 cpanm -V
 
-echo "## git status"
-git status
-echo "## git branch"
-git branch --all
-echo "## git diff --name-only origin/master"
-git diff --name-only origin/master
+echo "## Running cpanm --installdeps ."
+cpanm --installdeps --force --notest . 
 
-# echo "## Running cpanm --installdeps ."
-# cpanm --installdeps --force --notest . 
-#
-# export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-#
-# export PERL5LIB="${GITHUB_WORKSPACE}/modules"
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+export PERL5LIB="${GITHUB_WORKSPACE}/modules"
+FILES=`git diff --name-only origin/master | grep -P "(\.pl|\.pm|\.cgi)$"`
 
-# echo "## Running perlcritic"
-# find . -regex '.*\.\(pl\|pm\|cgi\)' -exec perlcritic --gentle --profile /.perlcriticrc {} \; |
-#    reviewdog -name="perlcritic" -filter-mode=file -efm="%f:%l:%c:%m" -reporter="github-pr-check"
+echo "## Running perlcritic"
+perlcritic --gentle --profile /.perlcriticrc $FILES |
+    reviewdog -name="perlcritic" -filter-mode=file -efm="%f:%l:%c:%m" -reporter="github-pr-check"
 
-# #export ESC_GITHUB_WORKSPACE=$(echo "$GITHUB_WORKSPACE" | perl -pe 's/\//\\\//g')
+#export ESC_GITHUB_WORKSPACE=$(echo "$GITHUB_WORKSPACE" | perl -pe 's/\//\\\//g')
 
-# # SUBSTR below puts the "perl -c format" into "file:line:error" format for reviewdog.
-# # (Also trims ./ or ../../ or /somedir/ from beginning of file path.)
-# export SUBSTR="s/(.*) at (.\/|\/github\/workspace\/)(.*) line (\d+)(.*)/\$3:\$4:\$1/g"
+# SUBSTR below puts the "perl -c format" into "file:line:error" format for reviewdog.
+# (Also trims ./ or ../../ or /somedir/ from beginning of file path.)
+export SUBSTR="s/(.*) at (.\/|\/github\/workspace\/)(.*) line (\d+)(.*)/\$3:\$4:\$1/g"
 
-# echo "## Running perl -c (on *.pm)"
-# find . -regex '.*\.\(pl\|pm\|cgi\)' -exec perl -c {} 2>&1 \; |
-#    grep -v " syntax OK" |
-#    perl -pe "$SUBSTR" |
-#    reviewdog -name="perl-syntax" -filter-mode=file  -efm="%f:%l:%m" -reporter="github-pr-check"
+echo "## Running perl -c (on *.pm)"
+temp_file=$(mktemp)
+
+for x in $FILES
+do
+   perl -c $x |& grep -v " syntax OK" | perl -pe "$SUBSTR" >>$temp_file
+done
+
+cat ${temp_file} |
+   reviewdog -name="perl-syntax" -filter-mode=file  -efm="%f:%l:%m" -reporter="github-pr-check"
+
+rm ${temp_file}
